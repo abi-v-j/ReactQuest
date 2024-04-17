8ccb135c725f3579a1718ccabb3a1da8fb37003f
abi-v-j <abijoy611@gmail.com>
2024-04-17 23:21:05 +0530

    latest

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,14 @@
-import Navbar from "../layouts/Navbar"
+import { Fragment } from "react/jsx-runtime";
+import Navbar from "../layouts/Navbar";
+import Card from "@mui/material/Card";
 
 const Home = () => {
   return (
-    <Navbar/>
-  )
-}
+    <Fragment>
+      <Navbar />
+      <Card>Hello</Card>
+    </Fragment>
+  );
+};
 
-export default Home
+export default Home;
